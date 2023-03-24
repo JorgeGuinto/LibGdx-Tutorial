dfdc292b96409667db565ad30d43e7cf9dd9c1c0
Jorge Guinto <jorgeguintop@gmail.com>
2023-03-24 02:18:00 -0600

    Creating repository

---
+++ desktop/src/com/jorgeguinto/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jorgeguinto;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jorgeguinto.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("TutorialLibGdx");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
